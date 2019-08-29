a85a64898e659976edbb5bb6fecaf5961ed359dc
Vaibhav Sinha <43650684+VaibhavSinha1908@users.noreply.github.com>
2019-08-29 21:59:02 +1000

    Function Based Components

--- ReactJS/Modern-React-Redux-2019/components/src/index.js
+++ ReactJS/Modern-React-Redux-2019/components/src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          image={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 3:30PM"
+          comment="First comment on the blog and I appreciate the bold coverage of the topic."
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          image={faker.image.avatar()}
+          author="Peter"
+          timeAgo="Today at 4:14PM"
+          comment="I liked the author's point of view."
+        ></CommentDetail>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          image={faker.image.avatar()}
+          author="John"
+          timeAgo="Today at 5:00PM"
+          comment="This text was a drag."
+        ></CommentDetail>
+      </ApprovalCard>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
